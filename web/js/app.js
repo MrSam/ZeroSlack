d8e50f8f47a497b802e28ce92935087f8b463c85
Sam Hermans <sam@lumturio.com>
2016-05-13 12:21:51 +0300

    try some oauth stuff

--- web/js/app.js
+++ web/js/app.js
@@ -3,7 +3,10 @@ var ZeroSlack = angular.module('ZeroSlack',[]);
 ZeroSlack.controller('LoginController', ['$scope', function($scope) {
 
     $scope.submit = function() {
-        // this.key contains the key
+        window.location.href = "https://slack.com/oauth/authorize?" +
+            $.param({
+                client_id: "35817589173.42673138644",
+                scope: "client"
+            });
     }
-
 }]);
