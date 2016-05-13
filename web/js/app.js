1a36a39c9c23ae132491d391e3431ffa447f9e61
Sam Hermans <sam@lumturio.com>
2016-05-13 11:43:28 +0300

    Add app specific .js and .css

---
+++ web/js/app.js
@@ -0,0 +1,9 @@
+var ZeroSlack = angular.module('ZeroSlack',[]);
+
+ZeroSlack.controller('LoginController', ['$scope', function($scope) {
+
+    $scope.submit = function() {
+        // this.key contains the key
+    }
+
+}]);
