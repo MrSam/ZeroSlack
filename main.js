1cb9abe895c43633472e8cb6072e9b52823cd67d
Sam Hermans <sam@lumturio.com>
2016-05-18 17:34:21 +0300

    close when all windows are closed

--- main.js
+++ main.js
@@ -48,13 +48,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-    app.quit();
-}
-});
+app.on('window-all-closed', () => app.quit());
 
 app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
